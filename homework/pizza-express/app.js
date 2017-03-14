299de4455dcc003e1b30d76d3a9386c20c216a23
savvyscript <nm3142@hotmail.com>
2017-03-14 19:20:08 -0400

    refactored routes and added views

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -8,12 +8,18 @@ app.set("view engine", "hbs");
 app.set('views', './views');
 
 app.get('/', function(req, res) {
-    res.send("Welcome to Pizza Express!");
-});
+//     res.send("Welcome to Pizza Express!");
 
-app.get('/topping/:type', function(req, res, next) {
-    res.send(`${req.params.type} Pizza, Good choice!`);
+     res.render('index.hbs,' {
+     	data: "Welcome to Pizza Express"
+     });
+  }); 
 
+app.get('/toppings/:type', function(req, res, next) {
+    // res.send(`${req.params.type} Pizza, Good choice!`);
+        res.render('toppings.hbs', {
+        	  data: req.params.type + '' + ''
+        	});        
 });
 
 app.listen(port, function(){
@@ -24,5 +30,12 @@ app.listen(port, function(){
 
 app.get('/order/:amount/:size', function(req, res, next) {
 
-   res.send(` order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute! `);
+//    res.send(` order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute! `);
+      res.render('order.hbs', {
+      	data: req.params.amount + req.param.size  + '' + ''
+      });
 });
+
+
+
+
